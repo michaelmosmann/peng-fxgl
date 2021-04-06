f97b1ef356cd17f1b9efb9e2ff909d360920efd0
Michael Mosmann <michael@mosmann.de>
2021-04-06 07:22:50 +0200

    some basics

---
+++ src/main/kotlin/de/mosmann/peng/basics/BasicTasks.kt
@@ -0,0 +1,43 @@
+package de.mosmann.peng.basics
+
+object BasicTasks {
+
+    fun add(number: Int, otherNumber: Int): Int {
+        // zwei nummern addieren und das Ergebnis zurückgeben
+        return TODO()
+    }
+
+    // Hier die Dokumentation von Sprachkonstrukten
+    // die für die folgenden Aufgaben hilfreich sein könnten
+    // -> https://kotlinlang.org/docs/control-flow.html
+
+    fun min(number: Int, otherNumber: Int): Int {
+        // zwei nummern vergleichen
+        // die kleinere zurück geben
+        return TODO()
+    }
+
+    fun max(number: Int, otherNumber: Int): Int {
+        // zwei nummern vergleichen
+        // die größere zurück geben
+        return TODO()
+    }
+
+    fun firstCharacter(value: String): Char {
+        // gebe den ersten Buchstaben zurück
+        // in den meisten Programmiersprachen fängt man
+        // bei 0 zu zählen an, und nicht bei 1
+
+        // in Kotlin kann man bei den meisten Typen/Klassen
+        // die eine Reihe von Werten beinhalten über eine
+        // get()-Funktion auf einen Wert mit einem Index zugreifen
+        // folgende Schreibweisen sind dabei gleichbedeutend
+        // variable.get(index) == variable[index]
+        return TODO()
+    }
+
+    fun firstEntry(value: Array<Int>): Int {
+        // das Prinzip ist hier das selbe wie bei firstCharacter
+        return TODO()
+    }
+}
---
+++ src/test/kotlin/de/mosmann/peng/basics/BasicTasksTest.kt
@@ -0,0 +1,37 @@
+package de.mosmann.peng.basics
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class BasicTasksTest {
+
+    @Test
+    fun checkAddFunction() {
+        assertThat(BasicTasks.add(1,2)).isEqualTo(3)
+        assertThat(BasicTasks.add(6,1)).isEqualTo(7)
+    }
+
+    @Test
+    fun checkMinFunction() {
+        assertThat(BasicTasks.min(1,2)).isEqualTo(1)
+        assertThat(BasicTasks.min(6,1)).isEqualTo(1)
+    }
+
+    @Test
+    fun checkMaxFunction() {
+        assertThat(BasicTasks.max(1,2)).isEqualTo(2)
+        assertThat(BasicTasks.max(6,1)).isEqualTo(6)
+    }
+
+    @Test
+    fun checkFirstCharFunction() {
+        assertThat(BasicTasks.firstCharacter("Klaus")).isEqualTo('K')
+        assertThat(BasicTasks.firstCharacter("Susi")).isEqualTo('S')
+    }
+
+    @Test
+    fun checkFirstEntryFunction() {
+        assertThat(BasicTasks.firstEntry(arrayOf(1,2,3))).isEqualTo(1)
+        assertThat(BasicTasks.firstEntry(arrayOf(3,3,3))).isEqualTo(3)
+    }
+}
